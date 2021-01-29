93a6fc3d82a3ddfd81fd264173df2eb1f4e36126
Ganesh B Nalawade <ganesh634@gmail.com>
2021-01-29 10:00:30 +0530

    Fix review comments

--- plugins/sub_plugins/cli_parser/json_parser.py
+++ plugins/sub_plugins/cli_parser/json_parser.py
@@ -10,7 +10,7 @@ __metaclass__ = type
 DOCUMENTATION = """
     author: Bradley Thornton (@cidrblock)
     name: json
-    short_description: Define configurable options for B(json) sub-plugin of M(ansible.utils.cli_parse module
+    short_description: Define configurable options for B(json) sub-plugin of M(ansible.utils.cli_parse) module
     description:
     - This plugin documentation provides the configurable options that can be passed
       to the M(ansible.utils.cli_parse) plugins when I(ansible.utils.json) is used as a value for
