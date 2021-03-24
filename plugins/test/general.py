d6c50676cf6f8be1111351e5cf148c2d2e929a40
Priyam Sahoo <priyamsahoo.26@gmail.com>
2021-03-24 12:03:03 +0530

    Added resolvable test plugin

--- plugins/test/general.py
+++ plugins/test/general.py
@@ -17,91 +17,91 @@ DOCUMENTATION = """
 """
 
 
-import socket
+# import socket
 
-from ansible.errors import AnsibleError
-from ansible.module_utils.basic import missing_required_lib
+# from ansible.errors import AnsibleError
+# from ansible.module_utils.basic import missing_required_lib
 
-try:
-    import ipaddress
+# try:
+#     import ipaddress
 
-    HAS_IPADDRESS = True
-except ImportError:
-    HAS_IPADDRESS = False
-
-
-def _need_ipaddress(func):
-    def wrapper(*args, **kwargs):
-        if not HAS_IPADDRESS:
-            msg = "'{test}' requires 'ipaddress' {stnd}".format(
-                test=func.__name__,
-                stnd=missing_required_lib("ipaddress").replace(
-                    "module", "plugin"
-                ),
-            )
-            raise AnsibleError(msg)
-        return func(*args, **kwargs)
-
-    return wrapper
+#     HAS_IPADDRESS = True
+# except ImportError:
+#     HAS_IPADDRESS = False
 
 
-def disabled(str):
-    """ Case insensitive test for `disabled`<br/>`interface.ip_redirects is ansible.utils.disabled`
-    """
-    try:
-        return str.lower() == "disabled"
-    except Exception:
-        return False
+# def _need_ipaddress(func):
+#     def wrapper(*args, **kwargs):
+#         if not HAS_IPADDRESS:
+#             msg = "'{test}' requires 'ipaddress' {stnd}".format(
+#                 test=func.__name__,
+#                 stnd=missing_required_lib("ipaddress").replace(
+#                     "module", "plugin"
+#                 ),
+#             )
+#             raise AnsibleError(msg)
+#         return func(*args, **kwargs)
+
+#     return wrapper
 
 
-def down(str):
-    """ Case insensitive test for `down`<br/>`interface.oper_state is ansible.utils.down`
-    """
-    try:
-        return str.lower() == "down"
-    except Exception:
-        return False
+# def disabled(str):
+#     """ Case insensitive test for `disabled`<br/>`interface.ip_redirects is ansible.utils.disabled`
+#     """
+#     try:
+#         return str.lower() == "disabled"
+#     except Exception:
+#         return False
 
 
-def enabled(str):
-    """ Case insensitive test for `enabled`<br/>`interface.bpdu_guard is ansible.utils.enabled`
-    """
-    try:
-        return str.lower() == "enabled"
-    except Exception:
-        return False
+# def down(str):
+#     """ Case insensitive test for `down`<br/>`interface.oper_state is ansible.utils.down`
+#     """
+#     try:
+#         return str.lower() == "down"
+#     except Exception:
+#         return False
 
 
-@_need_ipaddress
-def resolvable(str):
-    """ Test if an IP or name can be resolved via /etc/hosts or DNS<br/>`'docs.ansible.com' is ansible.utils.resolvable`
-    """
-    try:
-        ipaddress.ip_address(str)
-        ip = True
-    except Exception:
-        ip = False
-    if ip:
-        try:
-            socket.gethostbyaddr(str)
-            return True
-        except Exception:
-            return False
-    else:
-        try:
-            socket.getaddrinfo(str, None)
-            return True
-        except Exception:
-            return False
+# def enabled(str):
+#     """ Case insensitive test for `enabled`<br/>`interface.bpdu_guard is ansible.utils.enabled`
+#     """
+#     try:
+#         return str.lower() == "enabled"
+#     except Exception:
+#         return False
 
 
-def up(str):
-    """ Case insensitive test for `up`<br/>`interface.admin_state is ansible.utils.up`
-    """
-    try:
-        return str.lower() == "up"
-    except Exception:
-        return False
+# @_need_ipaddress
+# def resolvable(str):
+#     """ Test if an IP or name can be resolved via /etc/hosts or DNS<br/>`'docs.ansible.com' is ansible.utils.resolvable`
+#     """
+#     try:
+#         ipaddress.ip_address(str)
+#         ip = True
+#     except Exception:
+#         ip = False
+#     if ip:
+#         try:
+#             socket.gethostbyaddr(str)
+#             return True
+#         except Exception:
+#             return False
+#     else:
+#         try:
+#             socket.getaddrinfo(str, None)
+#             return True
+#         except Exception:
+#             return False
+
+
+# def up(str):
+#     """ Case insensitive test for `up`<br/>`interface.admin_state is ansible.utils.up`
+#     """
+#     try:
+#         return str.lower() == "up"
+#     except Exception:
+#         return False
 
 
 class TestModule(object):
@@ -109,11 +109,11 @@ class TestModule(object):
     """
 
     test_map = {
-        "disabled": disabled,
-        "down": down,
-        "enabled": enabled,
-        "resolvable": resolvable,
-        "up": up,
+        # "disabled": disabled,
+        # "down": down,
+        # "enabled": enabled,
+        # "resolvable": resolvable,
+        # "up": up,
     }
 
     def tests(self):
