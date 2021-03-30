055a162174157b0537834dcef3b77675b7005bc2
ashwini-mhatre <mashu97@gmail.com>
2021-03-30 20:18:02 +0530

    Add Uts for xml_to_json and json_to_xml

--- tests/unit/module_utils/test_xml_to_json.py
+++ tests/unit/module_utils/test_xml_to_json.py
@@ -8,45 +8,39 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 import unittest
-
-from ansible.errors import AnsibleFilterError
+from ansible.errors import AnsibleError
 from ansible_collections.ansible.utils.plugins.filter.xml_to_json import xml_to_json
 
-DATA = {
-    "engine": "xmltodict"
-}
+INVALID_DATA = "<netconf-state xmlns=\"urn:ietf:params:xml:ns:yang:ietf-netconf-monitoring\">"
 
 VALID_DATA = "<netconf-state xmlns=\"urn:ietf:params:xml:ns:yang:ietf-netconf-monitoring\">" \
              "<schemas><schema/></schemas></netconf-state>"
+
+
 
 class TestXmlToJson(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_invalid_argspec(self):
+    def test_invalid_data(self):
         """Check passing invalid argspec"""
 
         # missing required arguments
-        args = [DATA]
+        args = [INVALID_DATA, "xmltodict"]
         kwargs = {}
-        with self.assertRaises(AnsibleFilterError) as error:
+        with self.assertRaises(AnsibleError) as error:
             xml_to_json(*args, **kwargs)
+        print(str(error.exception))
         self.assertIn(
-            "Missing either 'data' or 'criteria' value in filter input, refer 'ansible.utils.xml_to_json' filter",
+            "Error when using plugin 'xml_to_json': Input Xml is not valid",
             str(error.exception),
         )
 
-        # missing required arguments
-        with self.assertRaises(AnsibleFilterError) as error:
-            xml_to_json([DATA])
-        self.assertIn(
-            "Missing either 'data' or 'engine' ", str(error.exception)
-        )
-
-
     def test_valid_data(self):
         """Check passing valid data as per criteria"""
-
+        self.maxDiff = None
+        output = """{"netconf-state": {"@xmlns": "urn:ietf:params:xml:ns:yang:ietf-netconf-monitoring", 
+        "schemas": {"schema": null}}}"""
         args = [VALID_DATA, "xmltodict"]
         result = xml_to_json(*args)
-        self.assertEqual(result, [])
+        self.assertEqual(result, output)
