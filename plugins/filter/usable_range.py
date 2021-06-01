f011c13707f68690c8493a0912358d2ee27cf076
Priyam Sahoo <priyamsahoo.26@gmail.com>
2021-06-01 11:11:56 +0530

    Rename to 'usable_range' and use '_need_address' decorator

--- plugins/filter/usable_range.py
+++ plugins/filter/usable_range.py
@@ -4,20 +4,20 @@
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 """
-Filter plugin file for expand_usable_range
+Filter plugin file for usable_range
 """
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 from ipaddress import IPv4Network
 
 from ansible_collections.ansible.utils.plugins.plugin_utils.base.ipaddress_utils import (
-    _validate_args,
+    _validate_args, ip_network, _need_ipaddress
 )
 
 __metaclass__ = type
-
+# 2.3.0
 DOCUMENTATION = """
-    name: expand_usable_range
+    name: usable_range
     author: Priyam Sahoo (@priyamsahoo)
     version_added: "2.2.0"
     short_description: Expand the usable IP addresses
@@ -39,7 +39,7 @@ EXAMPLES = r"""
 
 - name: Expand and produce list of usable IP addresses in 10.0.0.0/28
   ansible.builtin.set_fact:
-    data: "{{ '10.0.0.0/28' | ansible.utils.expand_usable_range }}"
+    data: "{{ '10.0.0.0/28' | ansible.utils.usable_range }}"
 
 # TASK [Expand and produce list of usable IP addresses in 10.0.0.0/28] ************************
 # ok: [localhost] => {
@@ -71,7 +71,7 @@ EXAMPLES = r"""
 
 - name: Expand and produce list of usable IP addresses in 10.1.1.1
   ansible.builtin.set_fact:
-    data: "{{ '10.1.1.1' | ansible.utils.expand_usable_range }}"
+    data: "{{ '10.1.1.1' | ansible.utils.usable_range }}"
 
 # TASK [Expand and produce list of usable IP addresses in 10.1.1.1] ***************************
 # ok: [localhost] => {
@@ -98,48 +98,31 @@ RETURN = """
 from ansible.errors import AnsibleError, AnsibleFilterError
 from ansible.module_utils.basic import missing_required_lib
 from ansible.module_utils.common.text.converters import to_text
+from ansible.module_utils.six import ensure_text
 
-try:
-    import ipaddress
-    HAS_IPADDRESS = True
-except ImportError:
-    HAS_IPADDRESS = False
-
-
-def _expand_usable_range(ip):
+@_need_ipaddress
+def _usable_range(ip):
     """Expand the usable IP addresses"""
 
     params = {"ip": ip}
-    _validate_args("expand_usable_range", DOCUMENTATION, params)
+    _validate_args("usable_range", DOCUMENTATION, params)
 
-    if not HAS_IPADDRESS:
-        raise AnsibleError(missing_required_lib("ipaddress"))
+    # if not HAS_IPADDRESS:
+    #     raise AnsibleError(missing_required_lib("ipaddress"))
 
     try:
-        ips = [to_text(usable_ips) for usable_ips in ipaddress.IPv4Network(to_text(ip))]
-        no_of_ips = len(ips)
+        ips = [to_text(usable_ips) for usable_ips in IPv4Network(ensure_text(ip))]
+        no_of_ips = IPv4Network(ensure_text(ip)).num_addresses
     except Exception as e:
-        raise AnsibleFilterError("Error while using plugin 'expand_usable_range': {msg}".format(msg = to_text(e)))
+        raise AnsibleFilterError("Error while using plugin 'usable_range': {msg}".format(msg = to_text(e)))
     
     return {"usable_ips": ips, "number_of_ips": no_of_ips}
 
-    # if not HAS_NETADDR:
-    #     raise AnsibleError(missing_required_lib("netaddr"))
-
-    # try:
-    # ips = [usable_ips for usable_ips in netaddr.IPNetwork(ip)]
-    # no_of_ips = len(ips)
-    # except Exception as e:
-        # raise AnsibleFilterError(f"Error while using plugin 'expand_usable_range': {str(e)}")
-    
-    # return {"usable_ips": ips, "number_of_ips": no_of_ips}
-
-
 class FilterModule(object):
-    """ expand_usable_range  """
+    """ usable_range  """
 
     def filters(self):
 
         """a mapping of filter names to functions"""
-        return {"expand_usable_range": _expand_usable_range}
+        return {"usable_range": _usable_range}
 
--- tests/unit/plugins/filter/test_usable_range.py
+++ tests/unit/plugins/filter/test_usable_range.py
@@ -11,7 +11,7 @@ __metaclass__ = type
 import unittest
 from ansible.errors import AnsibleError
 from ansible.module_utils.common.text.converters import to_text
-from ansible_collections.ansible.utils.plugins.filter.expand_usable_range import _expand_usable_range
+from ansible_collections.ansible.utils.plugins.filter.usable_range import _usable_range
 
 INVALID_DATA = ['helloworld', '192.168.1.25/24', '192.0.2.0/23/24', '10.0.0.0/322']
 
@@ -23,7 +23,7 @@ VALID_OUTPUT_2 = {'number_of_ips': 16, 'usable_ips': ['192.0.2.0', '192.0.2.1', 
                                                         '192.0.2.8', '192.0.2.9', '192.0.2.10', '192.0.2.11', 
                                                         '192.0.2.12', '192.0.2.13', '192.0.2.14', '192.0.2.15']}
 
-class TestFromXml(unittest.TestCase):
+class TestUsableRange(unittest.TestCase):
     def setUp(self):
         pass
 
@@ -33,9 +33,9 @@ class TestFromXml(unittest.TestCase):
         # missing required arguments
         ip = ""
         with self.assertRaises(AnsibleError) as error:
-            _expand_usable_range(ip)
+            _usable_range(ip)
         self.assertIn(
-            "Error while using plugin 'expand_usable_range': Address cannot be empty",
+            "Address cannot be empty",
             str(error.exception),
         )
 
@@ -46,7 +46,7 @@ class TestFromXml(unittest.TestCase):
         
         ip = INVALID_DATA[0]
         with self.assertRaises(AnsibleError) as error:
-            _expand_usable_range(ip)
+            _usable_range(ip)
         self.assertIn(
             "Expected 4 octets",
             str(error.exception),
@@ -54,7 +54,7 @@ class TestFromXml(unittest.TestCase):
 
         ip = INVALID_DATA[1]
         with self.assertRaises(AnsibleError) as error:
-            _expand_usable_range(ip)
+            _usable_range(ip)
         self.assertIn(
             "{} has host bits set".format(ip),
             str(error.exception),
@@ -62,7 +62,7 @@ class TestFromXml(unittest.TestCase):
 
         ip = INVALID_DATA[2]
         with self.assertRaises(AnsibleError) as error:
-            _expand_usable_range(ip)
+            _usable_range(ip)
         self.assertIn(
             "Only one '/' permitted",
             str(error.exception),
@@ -70,7 +70,7 @@ class TestFromXml(unittest.TestCase):
 
         ip = INVALID_DATA[3]
         with self.assertRaises(AnsibleError) as error:
-            _expand_usable_range(ip)
+            _usable_range(ip)
         self.assertIn(
             "not a valid netmask",
             str(error.exception)
@@ -80,9 +80,9 @@ class TestFromXml(unittest.TestCase):
         """Check passing valid data as per criteria"""
         
         ip = VALID_DATA[0]
-        result = _expand_usable_range(ip)
+        result = _usable_range(ip)
         self.assertEqual(result, VALID_OUTPUT_1)
 
         ip = VALID_DATA[1]
-        result = _expand_usable_range(ip)
+        result = _usable_range(ip)
         self.assertEqual(result, VALID_OUTPUT_2)
