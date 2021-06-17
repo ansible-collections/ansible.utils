0a2e083c9b80a3a62e03b04b6293ae5452e46665
stoned <stoned.elipot@gmail.com>
2021-06-17 09:26:13 +0200

    Make to_paths handles empty list and mapping (#58)
    Make to_paths handles empty list and mapping
    
    Reviewed-by: https://github.com/apps/ansible-zuul

--- plugins/module_utils/common/to_paths.py
+++ plugins/module_utils/common/to_paths.py
@@ -22,26 +22,39 @@ def to_paths(var, prepend, wantlist):
     if prepend:
         var = {prepend: var}
 
-    out = {}
-
-    def flatten(data, name=""):
+    def flatten(data, name="", out=None):
+        if out is None:
+            out = {}
         if isinstance(data, (dict, Mapping, MutableMapping)):
-            for key, val in data.items():
-                if name:
-                    if re.match("^[a-zA-Z_][a-zA-Z0-9_]*$", key):
-                        nname = name + ".{key}".format(key=key)
+            if data:
+                for key, val in data.items():
+                    if name:
+                        if re.match("^[a-zA-Z_][a-zA-Z0-9_]*$", key):
+                            nname = name + ".{key}".format(key=key)
+                        else:
+                            nname = name + "['{key}']".format(key=key)
                     else:
-                        nname = name + "['{key}']".format(key=key)
-                else:
-                    nname = key
-                flatten(val, nname)
+                        nname = key
+                    flatten(val, nname, out)
+            elif name:
+                out[name] = {}
+            else:
+                out = {}
         elif isinstance(data, list):
-            for idx, val in enumerate(data):
-                flatten(val, "{name}[{idx}]".format(name=name, idx=idx))
+            if data:
+                for idx, val in enumerate(data):
+                    flatten(
+                        val, "{name}[{idx}]".format(name=name, idx=idx), out
+                    )
+            elif name:
+                out[name] = []
+            else:
+                out = []
         else:
             out[name] = data
+        return out
 
-    flatten(var)
+    out = flatten(var)
     if wantlist:
         return [out]
     return out
