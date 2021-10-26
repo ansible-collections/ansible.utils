025ac0ef8650d2c01ff3d3f8e11653e5a633e1d0
ashwini-mhatre <mashu97@gmail.com>
2021-10-26 12:26:24 +0530

    fix sanity

--- plugins/filter/get_path.py
+++ plugins/filter/get_path.py
@@ -148,6 +148,8 @@ EXAMPLES = r"""
 """
 
 from ansible.errors import AnsibleFilterError
+from jinja2.filters import environmentfilter
+
 from ansible_collections.ansible.utils.plugins.module_utils.common.get_path import (
     get_path,
 )
@@ -155,16 +157,6 @@ from ansible_collections.ansible.utils.plugins.module_utils.common.get_path impo
 from ansible_collections.ansible.utils.plugins.module_utils.common.argspec_validate import (
     AnsibleArgSpecValidator,
 )
-
-# patch Jinja2 >= 3.0 for backwards compatibility
-try:
-    import sys as _sys
-    from jinja2.filters import pass_environment as _passenv
-
-    _mod = _sys.modules["jinja2.filters"]
-    _mod.environmentfilter = _passenv
-except ImportError:
-    _sys = None
 
 
 @environmentfilter
