b296d99318a6dd326c2bfb613371e7afe4611ee4
ashwini-mhatre <mashu97@gmail.com>
2021-10-26 18:56:52 +0530

    Add backword comatibility for jinja2 newer version

--- plugins/filter/get_path.py
+++ plugins/filter/get_path.py
@@ -157,6 +157,15 @@ from ansible_collections.ansible.utils.plugins.module_utils.common.get_path impo
 from ansible_collections.ansible.utils.plugins.module_utils.common.argspec_validate import (
     AnsibleArgSpecValidator,
 )
+try:
+    import sys as _sys
+    from jinja2.filters import pass_context as _passctx, pass_environment as _passenv, pass_eval_context as _passevalctx
+    _mod = _sys.modules['jinja2.filters']
+    _mod.contextfilter = _passctx
+    _mod.environmentfilter = _passenv
+    _mod.evalcontextfilter = _passevalctx
+except ImportError:
+    _sys = None
 
 
 @environmentfilter
