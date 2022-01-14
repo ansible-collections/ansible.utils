46be955d68048a8faf134b5b815f46ca6d30887c
ashwini-mhatre <mashu97@gmail.com>
2022-01-14 15:55:05 +0530

    Address review comments

--- plugins/filter/ipv6.py
+++ plugins/filter/ipv6.py
@@ -27,9 +27,11 @@ except ImportError:
 
 try:
     import netaddr
+
+    HAS_NETADDR = True
 except ImportError:
     # in this case, we'll make the filters return error messages (see bottom)
-    netaddr = None
+    HAS_NETADDR = False
 else:
 
     class mac_linux(netaddr.mac_unix):
@@ -169,7 +171,7 @@ class FilterModule(object):
 
     def filters(self):
         """ ipv6 filter """
-        if netaddr:
+        if HAS_NETADDR:
             return self.filter_map
         else:
             return dict(
