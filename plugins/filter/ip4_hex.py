dd4c5729236bdefd688c89aaa813aa776367a462
ashwini-mhatre <mashu97@gmail.com>
2022-01-14 15:56:01 +0530

    Address review comments

--- plugins/filter/ip4_hex.py
+++ plugins/filter/ip4_hex.py
@@ -26,9 +26,11 @@ except ImportError:
 
 try:
     import netaddr
+
+    HAS_NETADDR = True
 except ImportError:
     # in this case, we'll make the filters return error messages (see bottom)
-    netaddr = None
+    HAS_NETADDR = False
 else:
 
     class mac_linux(netaddr.mac_unix):
@@ -125,7 +127,7 @@ class FilterModule(object):
 
     def filters(self):
         """ ip4_hex filter """
-        if netaddr:
+        if HAS_NETADDR:
             return self.filter_map
         else:
             return dict(
