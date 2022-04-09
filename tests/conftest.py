64753c7d25a50bf4807dfc56ebefe0bb742bb668
cidrblock <bthornto@redhat.com>
2022-04-09 03:10:03 -0700

    Sanity fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
 """The pytest conftest.py file is used to setup the collection for testing."""
 
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
 import os
 import sys
 import shutil
@@ -15,10 +19,14 @@ def get_collection_name():
     """Get the collection namespace and name from the galaxy.yml file."""
     info_file = "galaxy.yml"
 
-    with open(info_file, "r") as fd:
-        galaxy_info = yaml.safe_load(fd)
+    try:
+        with open(info_file, "r") as fd:
+            galaxy_info = yaml.safe_load(fd)
+    except FileNotFoundError:
+        return None, None
 
-    return galaxy_info['namespace'] ,galaxy_info['name']
+    return galaxy_info["namespace"], galaxy_info["name"]
+
 
 def pytest_sessionstart(session):
     """Patch the ansible collection finder such that it does not raise a value error.
@@ -28,6 +36,12 @@ def pytest_sessionstart(session):
     """
     # pylint: disable=protected-access
     # pylint: disable=unused-argument
+
+    namespace, name = get_collection_name()
+    if namespace is None or name is None:
+        # Tests may not being run from the root of the repo.
+        return
+
     monkeypatch = pytest.MonkeyPatch()
     original = _get_collection_metadata
 
@@ -45,25 +59,20 @@ def pytest_sessionstart(session):
     parent_directory = os.path.dirname(os.path.dirname(__file__))
     collections_dir = os.path.join(parent_directory, "collections")
 
-    namespace, name = get_collection_name()
-    name_dir = os.path.join(collections_dir, "ansible_collections", namespace, name)
+    name_dir = os.path.join(
+        collections_dir, "ansible_collections", namespace, name
+    )
 
     if collections_dir not in sys.path:
         monkeypatch.syspath_prepend(collections_dir)
+
+    # If it's here, we will trust it was from this
     if os.path.isdir(name_dir):
-        shutil.rmtree(collections_dir)
-       
+        return
+
     os.makedirs(name_dir, exist_ok=True)
 
     for entry in os.listdir(parent_directory):
         if entry == "collections":
             continue
         os.symlink(os.path.abspath(entry), os.path.join(name_dir, entry))
-
-def pytest_sessionfinish(session):
-    """Remove the collections directory"""
-    # pylint: disable=unused-argument
-
-    parent_directory = os.path.dirname(os.path.dirname(__file__))
-    collections_dir = os.path.join(parent_directory, "collections")
-    shutil.rmtree(collections_dir)
