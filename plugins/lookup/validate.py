40857fb805618d71e5693504bbf07fcc402ceaae
cidrblock <bthornto@redhat.com>
2022-05-13 14:05:02 -0700

    Fix for unpack

--- plugins/lookup/validate.py
+++ plugins/lookup/validate.py
@@ -109,7 +109,9 @@ class LookupModule(LookupBase):
         if kwargs.get("engine"):
             params.update({"engine": kwargs["engine"]})
 
-        schema = [v for k, v in globals() if k.lower() == "documentation"]
+        schema = [
+            v for k, v in globals().items() if k.lower() == "documentation"
+        ]
         valid, argspec_result, updated_params = check_argspec(
             schema=schema[0],
             name="validate lookup",
