8b85003eb14ab9d82155fbb3101514c1059667a8
cidrblock <bthornto@redhat.com>
2022-05-18 09:35:41 -0700

    Add integration test files

---
+++ tests/integration/test_integration.py
@@ -0,0 +1,43 @@
+import subprocess
+
+import pytest
+
+
+def run(localhost_project, environment):
+    __tracebackhide__ = True
+    args = [
+        "ansible-navigator",
+        "run",
+        str(localhost_project.playbook),
+        "--ee",
+        "false",
+        "--mode",
+        "stdout",
+        "--pas",
+        str(localhost_project.playbook_artifact),
+        "--ll",
+        "debug",
+        "--lf",
+        str(localhost_project.log_file),
+        "--skip-tags",
+        "local,nxapi",
+    ]
+    process = subprocess.run(
+        args=args,
+        env=environment,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        check=False,
+        shell=False,
+    )
+    if process.returncode:
+        print(process.stdout.decode("utf-8"))
+        print(process.stderr.decode("utf-8"))
+
+        pytest.fail(
+            reason=f"Integration test failed: {localhost_project.role}"
+        )
+
+
+def test_integration(localhost_project, environment):
+    run(localhost_project, environment)
