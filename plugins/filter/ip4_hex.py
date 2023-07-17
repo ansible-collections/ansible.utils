6b186d516fb23484f41bf0defbae54c65cdb1457
ashwini-mhatre <mashu97@gmail.com>
2023-07-17 19:07:07 +0530

    Fix bug in ipv4_hex filter.

--- plugins/filter/ip4_hex.py
+++ plugins/filter/ip4_hex.py
@@ -110,6 +110,11 @@ def _ip4_hex(*args, **kwargs):
 
 def ip4_hex(arg, delimiter=""):
     """Convert an IPv4 address to Hexadecimal notation"""
+    try:
+        ip = netaddr.IPAddress(arg)        
+    except (netaddr.AddrFormatError, ValueError):
+        msg = "You must pass a valid IP address; {0} is invalid".format(arg)
+        raise AnsibleFilterError(msg)
     numbers = list(map(int, arg.split(".")))
     return "{0:02x}{sep}{1:02x}{sep}{2:02x}{sep}{3:02x}".format(*numbers, sep=delimiter)
 
