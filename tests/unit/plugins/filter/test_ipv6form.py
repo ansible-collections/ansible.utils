4deddf2c3b88b0b1674945da00d9d606faec7a19
Ashwini Mhatre <mashu97@gmail.com>
2023-08-10 13:40:17 +0530

    Feature ipv6 format (#295)
    * Create ipv6form.py
    
    * Update ipv6form.py
    
    * Update ipv6form.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add code
    
    * Add docs
    
    * Add uts and integration tests. fix sanity
    
    * iFix sanity
    
    * Fix sanity
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Ashwini Mhatre <amhatre@amhatre-thinkpadt14sgen2i.pnq.csb>

---
+++ tests/unit/plugins/filter/test_ipv6form.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023 Red Hat
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""
+Unit test file for ipcut filter plugin
+"""
+
+from __future__ import absolute_import, division, print_function
+
+
+__metaclass__ = type
+
+import unittest
+
+from ansible_collections.ansible.utils.plugins.filter.ipv6form import _ipv6form
+
+
+class TestIpv6Form(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_expand(self):
+        """Expand given ipv6 address"""
+
+        args = ["", "1234:4321:abcd:dcba::17", "expand"]
+        result = _ipv6form(*args)
+        self.assertEqual(result, "1234:4321:abcd:dcba:0000:0000:0000:0017")
+
+    def test_compress(self):
+        """Compress given ipv6 address"""
+
+        args = ["", "1234:4321:abcd:dcba:0000:0000:0000:0017", "compress"]
+        result = _ipv6form(*args)
+        self.assertEqual(result, "1234:4321:abcd:dcba::17")
+
+    def test_x509(self):
+        """Compress given ipv6 address into x509 form"""
+
+        args = ["", "1234:4321:abcd:dcba::17", "x509"]
+        result = _ipv6form(*args)
+        self.assertEqual(result, "1234:4321:abcd:dcba:0:0:0:17")
