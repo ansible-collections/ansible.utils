d3b8745417882ca87f893e21794033d7997331b7
Ruchi Pakhle <ruchipakhle@gmail.com>
2025-06-09 00:01:10 +0530

    rm unused var

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -43,5 +43,5 @@ def run(localhost_project, environment):
         pytest.fail(reason=f"Integration test failed: {localhost_project.role}")
 
 
-def test_integration(localhost_project, environment, monkeypatch):
+def test_integration(localhost_project, environment):
     run(localhost_project, environment)
